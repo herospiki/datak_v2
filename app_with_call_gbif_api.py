ab1a11db323ef164f3822be42c20f82cc3c86ae8
herospiki <sophie.piekarec@gmail.com>
2023-05-21 17:11:21 +0200

    First commit

---
+++ app_with_call_gbif_api.py
@@ -0,0 +1,193 @@
+# streamlit run [app name]
+
+# Depuis une console :
+# streamlit run cv.py
+
+# Local URL: http://localhost:8501
+# Network URL: http://192.168.0.89:8501
+
+
+# Chargement des librairies
+
+import pandas as pd
+import geopandas as gpd 
+from shapely import wkt
+import streamlit as st
+import my_functions as mf
+from PIL import Image
+
+# Initial page config
+
+st.set_page_config(
+    page_title='GBIF',
+    layout="wide",
+    initial_sidebar_state="collapsed",
+)
+
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
+
+#local_css("style.css")
+
+
+# Chargement des éco-regions
+
+path_to_eco_regions_csv = "data\eco-regions-simplified.csv" 
+path_to_cixiidae_flow_csv = "data\cixiidae_flow.csv"
+ 
+
+features_to_keep = ['key', 'basisOfRecord', 'individualCount', 'scientificName', 'acceptedScientificName', 'kingdom', 'phylum',
+       'order', 'family', 'genus', 'species', 'genericName', 'specificEpithet',
+       'taxonRank', 'taxonomicStatus', 'iucnRedListCategory','decimalLongitude', 'decimalLatitude', 
+       'continent', 'stateProvince','year','countryCode',
+       'country','coordinateUncertaintyInMeters', 'lifeStage',
+       'occurrenceRemarks', 'identificationRemarks']
+
+
+@st.cache_resource # 👈 Add the caching decorator
+def load_eco_regions(path_to_eco_regions_csv):
+    eco_regions_df = pd.read_csv(path_to_eco_regions_csv)
+    eco_regions_df['geometry'] = eco_regions_df['geometry'].apply(wkt.loads)
+    eco_regions_df = gpd.GeoDataFrame(eco_regions_df, crs='epsg:4326')
+    return eco_regions_df
+
+#@st.cache_resource # 👈 Add the caching decorator
+def load_flow_data(path_to_cixiidae_flow_csv):
+    flow_df = pd.read_csv(path_to_cixiidae_flow_csv,sep=';')
+    return flow_df
+
+eco_regions_df = load_eco_regions(path_to_eco_regions_csv)
+flow_df = load_flow_data(path_to_cixiidae_flow_csv)
+
+list_genus = set(flow_df['genus'].values)
+
+# Choix du genre
+def panel_choix_genus():
+    with st.container() :
+        selected_genus = st.selectbox(label='Genre', options= list_genus, key='selected_genus')
+        return selected_genus
+
+def panel_choix_species(debug_mode,genus):
+    list_species = set(flow_df[flow_df['genus'] == genus]['species'].values)
+    rank = 'species'
+    eco_regions_gbif_found_df = pd.DataFrame()
+    eco_regions_flow_found_df = pd.DataFrame()
+    geo_occ_df = pd.DataFrame()
+    flow_occ_df = pd.DataFrame()
+    with st.container() :
+        with st.form('species selection'):
+            searched_name = st.selectbox(label='Espèce', options= list_species, key='searched_name')
+            submitted = st.form_submit_button('Submit')
+            if submitted :
+                st.write('You selected:',  searched_name) 
+                searched_name = 0 if st.session_state.searched_name == '0' else st.session_state.searched_name
+                name_backbone, dict_results = mf.search_gbif_from_name_and_rank(searched_name,rank)
+                geo_occ_df = mf.build_geo_df(dict_results, features_to_keep, eco_regions_df.crs)
+                flow_occ_df, eco_regions_flow_found_df = mf.find_eco_regions_for_flow_species(flow_df[flow_df['species']== searched_name], eco_regions_df)
+                
+                if (geo_occ_df.size == 0) :
+                    body =  "No occurrence of " + searched_name + " with coordinates was found in the GBIF database"
+                    st.warning(body, icon="😢")
+                    print(flow_occ_df)
+                else : 
+                    eco_regions_gbif_found_df = mf.find_eco_regions(geo_occ_df, eco_regions_df)
+                if debug_mode :
+                    st.markdown(name_backbone) 
+        return eco_regions_flow_found_df, eco_regions_gbif_found_df, geo_occ_df, flow_occ_df
+
+def panel_gbif_comment(eco_regions_gbif_found_df, geo_occ_df):
+    eco_regions_found_data = eco_regions_gbif_found_df['ECO_NAME'].value_counts().reset_index(). \
+                        rename(columns={'index': 'Name', 'ECO_NAME': 'Number of occurrences'})
+    line1 = st.session_state.searched_name + " has been found "+ str(eco_regions_gbif_found_df.shape[0]) + " times in the GBIF database"
+    line2 = f"They have been found in {len(eco_regions_found_data['Name'])} eco-regions "
+    line3 = eco_regions_found_data['Name'].unique()
+    with st.container():
+        col1, col2 = st.columns(2)
+        with col1:
+           st.markdown(line1)
+           st.markdown(line2)
+           st.markdown(line3)
+    
+        with col2:
+             st.bar_chart(eco_regions_found_data, x = 'Name', y ='Number of occurrences', width=400, height=400, use_container_width= False)
+    
+    return eco_regions_gbif_found_df,geo_occ_df
+
+
+def panel_flow_comment(eco_regions_flow_found_df, flow_occ_df):
+    eco_regions_flow_found_df = eco_regions_flow_found_df.drop_duplicates()
+    eco_regions_found_data = eco_regions_flow_found_df['ECO_NAME'].value_counts().reset_index(). \
+                        rename(columns={'index': 'Name', 'ECO_NAME': 'Number of occurrences'})
+    line1 = st.session_state.searched_name + " has been found "+ str(eco_regions_flow_found_df.shape[0]) + " times in the FLOW database"
+    line2 = f"They have been found in {len(eco_regions_found_data['Name'])} eco-regions "
+    line3 = eco_regions_found_data['Name'].unique()
+
+    with st.container():
+        st.markdown("Dans la base FLOW")
+        col1, col2 = st.columns(2)
+        with col1:
+           st.markdown(line1)
+           st.markdown(line2)
+           st.markdown(line3)
+    
+        with col2:
+             st.bar_chart(eco_regions_found_data, x = 'Name', y ='Number of occurrences', width=400, height=400, use_container_width= False)
+    return eco_regions_flow_found_df
+
+
+def show_map(eco_regions_found_df,geo_occ_df):
+    map = mf.create_map_eco_regions(eco_regions_found_df,geo_occ_df)
+    with st.container():
+        map_html = map._repr_html_()
+        st.components.v1.html(map_html, height=900, width=1000)
+
+
+def hc_header():
+    
+    st.header('GBIF | Global Biodiversity Information Facility occurrences and WWF eco-regions')
+    st.header('Animalia | Arthropoda | Insecta | Hemiptera | Cixiidae')
+    #st.image()
+    st.markdown('**Data For Good**')
+    st.write('-----------------')
+
+    #https://www.gbif.org/occurrence/map?has_coordinate=true&has_geospatial_issue=false&taxon_key=8470
+
+    #GBIF.org (19 May 2023) GBIF Occurrence Download https://doi.org/10.15468/dl.86cbu4 
+
+def hc_sidebar():
+    st.sidebar.header('Cixiidae')
+    image = Image.open('images\cixiidae.png')
+    st.sidebar.image(image)
+    st.sidebar.markdown('Data For Good')
+    st.sidebar.markdown(
+        '''Link to Streamlit doc :  https://docs.streamlit.io/''')
+    
+def hc_body():
+    debug_mode = st.checkbox('debug mode')
+    tab1, tab2 = st.tabs(
+        ["Where are they ?", "Chatbox"])
+    with tab1:
+        genus = panel_choix_genus()
+        eco_regions_flow_found_df, eco_regions_gbif_found_df, geo_occ_df,flow_occ_df = panel_choix_species(debug_mode,genus)
+        if (eco_regions_gbif_found_df.size != 0) : 
+            panel_gbif_comment(eco_regions_gbif_found_df, geo_occ_df)
+            show_map(eco_regions_gbif_found_df, geo_occ_df)
+        if (eco_regions_flow_found_df.size != 0): 
+            panel_flow_comment(eco_regions_flow_found_df, flow_occ_df)
+
+    with tab2:
+        ### Ici 
+        st.markdown('TBC')
+
+
+def main():
+    hc_sidebar()
+    hc_header()
+    hc_body()
+
+
+# Run main()
+
+if __name__ == '__main__':
+    main()
---
+++ my_functions.py
@@ -0,0 +1,162 @@
+from shapely.geometry import Polygon, Point
+from shapely import centroid
+from pygbif import occurrences
+from pygbif import species
+import folium
+from folium import Popup
+import branca.colormap as cm
+import pandas as pd
+import geopandas as gpd
+import numpy as np
+
+# Fonction de recherche à partir d'un nom d'espèce ou de genre
+# rank = species ou genus ou family
+
+def search_gbif_from_name_and_rank(searched_name, rank):
+    print(searched_name)
+    name_backbone = species.name_backbone(searched_name, rank=rank, verbose=True) 
+    rank_key = rank+"Key"
+    if rank_key in name_backbone :
+        key_number = name_backbone[rank_key]
+        if rank_key == 'speciesKey' :
+            results = occurrences.search(speciesKey = key_number, hasCoordinate=True)
+        if rank_key == 'familyKey' :
+            results = occurrences.search(familyKey = key_number, hasCoordinate=True)
+        if rank_key == 'genusKey' :
+            results = occurrences.search(genusKey = key_number, hasCoordinate=True)  
+        return name_backbone, results
+    else : 
+        return name_backbone, "Not Found"
+
+
+def build_geo_df(dict_results, features_to_keep, crs):
+    # build empty dataframe
+    if (dict_results == 'Not Found') : 
+         return pd.DataFrame()
+    else : 
+        df_results = pd.DataFrame(dict_results['results'])
+        if df_results.size == 0 :
+             return pd.DataFrame()
+        else :
+             # Toutes les colonnes ne sont pas présentes à chaque fois...
+            df_results = df_results.reindex(columns=features_to_keep)
+            partial_data_df = df_results[features_to_keep]
+             # Suppression des duplicats
+            partial_data_df = partial_data_df.set_index('key').drop_duplicates()
+             # Suppression des coordonnées à (0,0)
+            partial_data_df = partial_data_df[(partial_data_df['decimalLongitude'] != 0)  
+                                               &  (partial_data_df['decimalLatitude']!= 0)]
+             # Extraire la liste et rajouter une colonne nommée geometry avec les Point pour chaque occurrence
+            #  'decimalLongitude', 'decimalLatitude'
+            geometry = [Point(xy) for xy in zip(partial_data_df['decimalLongitude'], partial_data_df['decimalLatitude'])]
+            geo_occ_df = gpd.GeoDataFrame(partial_data_df, #specify our data
+                        geometry=geometry, crs=crs) #specify the geometry list we create
+            return geo_occ_df
+                    
+def find_eco_regions(geo_occ_df, eco_regions_df):
+    geo_occ_df = geo_occ_df.drop_duplicates()
+    if (geo_occ_df.size > 0) :
+    # Trouver les éco-régions auxquelles appartiennent les occurrences (avec leurs coordonnées GPS)
+        pointsInEcoregions = gpd.sjoin(geo_occ_df, eco_regions_df, predicate="within", how='left')
+        pointsInEcoregions['species'] = pointsInEcoregions['species'].fillna('Unknown species')
+        pointsInEcoregions['genus'] = pointsInEcoregions['genus'].fillna('Unknown genus')
+        pointsInEcoregions['ECO_NAME'] = pointsInEcoregions['ECO_NAME'].fillna('Eco-region not identified')
+        pointsInEcoregions['ECO_ID'] = pointsInEcoregions['ECO_ID'].fillna(0)
+        df = pointsInEcoregions[['ECO_NAME', 'ECO_ID']].merge(eco_regions_df[['ECO_ID','geometry']], on='ECO_ID', how='left')
+        return df
+
+def find_eco_regions_for_flow_species(flow_df, eco_regions_df):
+    flow_df = flow_df.drop_duplicates()
+    if (flow_df.size > 0) :
+        geometry = [Point(xy) for xy in zip(flow_df['longitude'], flow_df['latitude'])]
+        flow_occ_df = gpd.GeoDataFrame(flow_df, #specify our data
+                        geometry=geometry, crs=eco_regions_df.crs) #specify the geometry list we create
+    # Trouver les éco-régions auxquelles appartiennent les occurrences (avec leurs coordonnées GPS)
+        pointsInEcoregions = gpd.sjoin( flow_occ_df, eco_regions_df, predicate="within", how='left')
+        pointsInEcoregions['species'] = pointsInEcoregions['species'].fillna('Unknown species')
+        pointsInEcoregions['genus'] = pointsInEcoregions['genus'].fillna('Unknown genus')
+        pointsInEcoregions['ECO_NAME'] = pointsInEcoregions['ECO_NAME'].fillna('Eco-region not identified')
+        pointsInEcoregions['ECO_ID'] = pointsInEcoregions['ECO_ID'].fillna(0)
+        df = pointsInEcoregions[['ECO_NAME', 'ECO_ID']].merge(eco_regions_df[['ECO_ID','geometry']], on='ECO_ID', how='left')
+        return pointsInEcoregions, df
+
+def get_center_coordinate(coordinates):
+ 
+    # Calculate the average latitude and longitude of the two points
+    latitude = (coordinates[0][0] + coordinates[1][0]) / 2
+    longitude = (coordinates[0][1] + coordinates[1][1]) / 2
+
+    return latitude, longitude
+
+def get_triangle_center(vertices):
+ 
+    # Calculate the average latitude and longitude of the vertices
+    avg_latitude = sum(point[0] for point in vertices) / len(vertices)
+    avg_longitude = sum(point[1] for point in vertices) / len(vertices)
+
+    return avg_latitude, avg_longitude
+
+def get_centroid(points):  
+    # Extract (latitude, longitude) tuples from dataframe
+    lat_lon_list = list(zip(points['decimalLatitude'].tolist(), points['decimalLongitude'].tolist()))
+    if len(lat_lon_list) == 1 :
+        return lat_lon_list[0]
+    if len(lat_lon_list) == 2 :
+        return get_center_coordinate(lat_lon_list)
+    if len(lat_lon_list) == 3 :
+        return get_triangle_center(lat_lon_list)   
+    else : 
+        poly = Polygon(lat_lon_list)
+        print(centroid(poly))
+        return centroid(poly).y, centroid(poly).x
+
+
+def create_map_eco_regions(df, geo_occ_df):
+    # Adapter le code pour le cas où on a des milliers d'occurrences...
+
+    # Calculer le centroïde des points pour centrer la carte d'emblée 
+    lat,long =  get_centroid(geo_occ_df)
+    min_year = geo_occ_df['year'].min()
+    max_year = geo_occ_df['year'].max()
+    map = folium.Map(location=[lat, long], zoom_start=4)
+
+    style_function = lambda x: {'fillColor': '#ffffff', 
+                            'color':'#000000', 
+                            'fillOpacity': 1, 
+                            'weight': 1}
+    highlight_function = lambda x: {'fillColor': '#000000', 
+                                'color':'#000000', 
+                                'fillOpacity': 0.50, 
+                                'weight': 0.1}
+    # Add a GeoJson layer with the polygons : list of eco-regions['ECO_NAME','geometry']
+
+    colormap = cm.LinearColormap(colors=['red','yellow','green'],
+                               vmin=min_year, vmax=max_year)
+    for row in df.itertuples():
+           if row.geometry != None :
+             folium.GeoJson(row.geometry, name= row.ECO_NAME,
+                          style_function=style_function, 
+                          highlight_function=highlight_function, 
+                          popup=Popup(row.ECO_NAME)).add_to(map)
+           
+    for row in geo_occ_df.itertuples(index=False):
+        if (row.year != np.nan) : 
+            #print(row)
+    # Add a Marker layer with the points
+            folium.Circle(radius=200,
+                 location=(row.decimalLatitude,row.decimalLongitude),
+                 popup=Popup(row.species + " " + str(row.year)),
+                 color = colormap(row.year),
+                 legend_name='year',
+                 fill=True).add_to(map)
+            
+        else :
+             folium.Circle(radius=200,
+             location=(row.decimalLatitude,row.decimalLongitude),
+             popup=Popup(row.species),
+             color = 'blue',
+             fill=True).add_to(map)
+             
+    colormap.caption = 'Year of occurrence'
+    colormap.add_to(map)
+    return map
